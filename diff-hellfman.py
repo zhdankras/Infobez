33658d4e3aa17017563b04b15a439e712e70f22a
zhdankras <artem@artem-mbp.local>
2022-01-24 23:05:51 +0300

    new

--- diff-hellfman.py
+++ diff-hellfman.py
@@ -1,5 +1,7 @@
 g = int(input("Введите общее число g: "))
 p = int(input("Введите общее число p: "))
+a = int(input("Введите число для Алисы: "))
+b = int(input("Введите число для Боба: "))
 
 def get_number_Alice(a):
     return (g ** a) % p
@@ -7,6 +9,14 @@ def get_number_Alice(a):
 def get_number_Bob(b):
     return (g ** b) % p
 
-print("Число Элис: ", get_number_Alice(23))
-print("Число Боба: ", get_number_Bob(12))
-print(get_number_Alice(23) == get_number_Bob(12))
+A = get_number_Alice(a)
+B = get_number_Bob(b)
+
+def get_parse_number_Alice(A):
+    return (A ** b) % p
+
+def get_parse_number_Bob(B):
+    return (B ** a) % p
+
+if get_parse_number_Alice(A) == get_number_Bob(B):
+    print("Общее число, равно: ", get_parse_number_Alice(A))
